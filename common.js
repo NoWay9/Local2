3f3d0c40fac126baf7be83473ce437517a1bed60
Liubomyr <strukliubomyr@gmail.com>
2018-01-18 20:54:36 +0200

    Format code

--- common.js
+++ common.js
@@ -1,32 +1,32 @@
-function loadLanguage(){
-var defaultLanguage = 'ua';   
-var language = localStorage.getItem('language') || defaultLanguage;
-    var $radio = document.querySelectorAll('#languages input[data-language="' + language + '"]')[0];
-    $radio.checked = true;
-updateText(language);
-}
-
-function saveLanguage(language){
-localStorage.setItem('language', language);
-updateText(language);
-}
-
-function updateText(language){
-    document.body.setAttribute('data-current-language',language);
-}
-
-document.querySelectorAll('#languages input[type="radio"]').forEach(function($radio){
-  $radio.addEventListener('change', function(e){
-  var language = e.target.getAttribute('data-language');
+function loadLanguage() {
+  var defaultLanguage = 'ua';
+  var language = localStorage.getItem('language') || defaultLanguage;
+  var $radio = document.querySelectorAll('#languages input[data-language="' + language + '"]')[0];
+  $radio.checked = true;
   updateText(language);
-});
+}
+
+function saveLanguage(language) {
+  localStorage.setItem('language', language);
+  updateText(language);
+}
+
+function updateText(language) {
+  document.body.setAttribute('data-current-language', language);
+}
+
+document.querySelectorAll('#languages input[type="radio"]').forEach(function ($radio) {
+  $radio.addEventListener('change', function (e) {
+    var language = e.target.getAttribute('data-language');
+    updateText(language);
+  });
 });
 
 
-document.querySelector('#save').addEventListener('click', function(e){
-e.preventDefault();
-var language = document.querySelector('#languages input:checked').getAttribute('data-language');
-saveLanguage(language);
+document.querySelector('#save').addEventListener('click', function (e) {
+  e.preventDefault();
+  var language = document.querySelector('#languages input:checked').getAttribute('data-language');
+  saveLanguage(language);
 })
 addEventListener('load', loadLanguage);
 
